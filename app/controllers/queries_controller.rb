e98823e5d5ed9ff34af5b1b7cc72f350ca1c4376
Marc-André Giroux <mgiroux0@gmail.com>
2016-02-21 15:32:50 -0500

    bump gems

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -5,7 +5,7 @@ class QueriesController < ApplicationController
   def create
     query_string = params[:query]
     query_variables = params[:variables] || {}
-    query = GraphQL::Query.new(BlogSchema, query_string, variables: query_variables, debug: true)
-    render json: query.result
+    result = BlogSchema.execute(query_string, variables: query_variables)
+    render json: result
   end
 end
--- app/graph/types/query_type.rb
+++ app/graph/types/query_type.rb
@@ -1,12 +1,12 @@
-QueryType = GraphQL::ObjectType.define do
-  name "Query"
-  description "The query root for this schema"
+  QueryType = GraphQL::ObjectType.define do
+    name "Query"
+    description "The query root for this schema"
 
-  field :blog do
-    type BlogType
-    argument :id, !types.ID
-    resolve -> (obj, args, ctx) {
-      Blog.find(args[:id])
-    }
+    field :blog do
+      type BlogType
+      argument :id, !types.ID
+      resolve -> (obj, args, ctx) {
+        Blog.find(args[:id])
+      }
+    end
   end
-end
